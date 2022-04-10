b3ea37ab504a1cb7a2d391e1ac66db91de41d52c
Mu-tor <zzfsky@126.com>
2022-04-11 00:13:37 +0800

    完成管理员界面“查看记录”功能（显示驳回预约的处理员因数据不足暂未添加，暂时显示为隐藏）

--- EquipManageSystem/controller/admin.py
+++ EquipManageSystem/controller/admin.py
@@ -5,6 +5,7 @@ from modules.admins import Admins
 from modules.booking import Booking
 from modules.brodtl import Brodtl
 from modules.equipment import Equipment
+from modules.record import Record
 from modules.users import Users
 
 admin = Blueprint("admin", __name__)
@@ -30,18 +31,16 @@ def admin_message_num():  # 待处理事务数
     return len(wait_process())
 
 
-@admin.route('/approval', methods=['GET', 'POST'])  # 审核
-def admin_approval():
+def get_details(works):  # 获得记录详情
     brotli = Brodtl()
     addr = Address()
     users = Users()
     equip = Equipment()
     results = []
-    bookings = wait_process()
-    for booking in bookings:
-        details = brotli.find_book_by_bid(booking.bid)  # 详情
-        user = users.find_user_by_id(booking.uid)
-        result = {"booking": booking, "details": details, "user": user}
+    for work in works:
+        details = brotli.find_book_by_bid(work.bid)  # 详情
+        user = users.find_user_by_id(work.uid)
+        result = {"booking": work, "details": details, "user": user}
         if details.is_addr == 1:
             row = addr.find_addr_by_id(details.addrid)
             result.setdefault("address", row)
@@ -49,7 +48,30 @@ def admin_approval():
             row = equip.find_eqp_by_id(details.eqpid)
             result.setdefault("equipment", row)
         results.append(result)
+    return results
+
+
+@admin.route('/approval', methods=['GET', 'POST'])  # 审核
+def admin_approval():
+    bookings = wait_process()  # 获取待处理事务
+    results = get_details(bookings)
     return render_template("ProcessApprovals.html", results=results, admin=session.get('admin'))
+
+
+@admin.route('/look_record', methods=['GET', 'POST'])  # 查看所有记录
+def admin_look_record():
+    book = Booking()
+    record = Record()
+    admins = Admins()
+    bookings = book.find_all()  # 获取所有记录
+    results = get_details(bookings)
+    for result in results:
+        if result['booking'].is_agree == 1:  # 填加处理人信息和记录表信息
+            recd = record.find_rec_by_bid(result['booking'].bid)
+            result.setdefault("record", recd)
+            adm = admins.find_admin_by_id(recd.admid)
+            result.setdefault("admin", adm)
+    return render_template("ManagerLookRecord.html", results=results, admin=session.get('admin'))
 
 
 @admin.before_request
